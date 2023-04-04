428d378f550ccbb56376890b4bb577dc281982b4
nikitamitroshin <mitroshin.js@gmail.com>
2023-04-04 19:19:16 +0300

    add ios splashscreen

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import SplashScreen from 'react-native-splash-screen'
 
 import ScreensStack from 'navigation/components/ScreensStack'
 
@@ -10,7 +11,7 @@ const Root: React.FC = () => (
   <>
     <StatusBar barStyle='light-content' />
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer onReady={SplashScreen.hide}>
         <ScreensStack />
       </NavigationContainer>
     </SafeAreaProvider>
