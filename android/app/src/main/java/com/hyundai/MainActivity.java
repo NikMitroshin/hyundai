65e2dd511db64859b42c71712f50a461633985d5
nikitamitroshin <mitroshin.js@gmail.com>
2023-04-05 10:05:01 +0300

    add android splashscreen

--- android/app/src/main/java/com/hyundai/MainActivity.java
+++ android/app/src/main/java/com/hyundai/MainActivity.java
@@ -3,6 +3,7 @@ package com.hyundai;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
@@ -19,6 +20,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
--- src/screens/ActionsScreen/ActionsScreen.tsx
+++ src/screens/ActionsScreen/ActionsScreen.tsx
@@ -4,6 +4,8 @@ import { Text } from 'react-native'
 
 import { StackNavProp, TabRouteNames } from 'types'
 
-const ActionsScreen: React.FC<StackNavProp<TabRouteNames.ACTIONS_SCREEN>> = () => <Text>ACTIONS_SCREEN</Text>
+const ActionsScreen: React.FC<StackNavProp<TabRouteNames.ACTIONS_SCREEN>> = () => (
+  <Text>Детальный экран github action ??</Text>
+)
 
 export default ActionsScreen
